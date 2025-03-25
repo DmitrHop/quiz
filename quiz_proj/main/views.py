7b7937314b211141c6c3da42ca409bcdec7636ee
DmitrHop <bimagarkovenko@gmail.com>
2025-03-25 15:59:16 +0500

    начало работы с неисчисляемым количеством url аддресов

--- quiz_proj/main/views.py
+++ quiz_proj/main/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from main.models import Quiz, Answer, Question
 
 def main(request):
     return render(request, 'main.html')
 # Create your views here.
+
+def quiz(request):
+    all_quiz = Quiz.objects.all()
+
+    return render (request, 'quiz.html', {'data':all_quiz})
--- quiz_proj/quiz_proj/settings.py
+++ quiz_proj/quiz_proj/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIR = [
-    BASE_DIR / "static/",
+    BASE_DIR / "static",
     
 ]
 
