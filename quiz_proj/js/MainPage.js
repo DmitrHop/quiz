1fea7c9bf58e6dca06740f44041e5bf7a5943969
Denis Ogay <ogay-den@mail.ru>
2025-04-08 22:53:18 +0500

    Main page

---
+++ quiz_proj/js/MainPage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const MainPage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Quiz App</h1>
+      <nav>
+        <ul>
+          <li><Link to="/quizzes">View Quizzes</Link></li>
+          <li><Link to="/teacher">Teacher's Dashboard</Link></li>
+          <li><Link to="/profile">My Profile</Link></li>
+          <li><Link to="/login">Log In</Link></li>
+          <li><Link to="/signup">Sign Up</Link></li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default MainPage;
